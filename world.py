5f7f960f971be8aa01564770c4d69c0718adc8e8
VincenzoCastrignano <vincenzo.castrignano@esqese.ucly.fr>
2022-05-13 12:00:16 +0200

    Fin première séance

--- world.py
+++ world.py
@@ -2,17 +2,21 @@
 # Olivier Georgeon, 2021.
 # This code is used to teach Developmental AI.
 # from turtlesim_enacter import TurtleSimEnacter # requires ROS
+import random
+
 from turtlepy_enacter import TurtlePyEnacter
 # from Agent5 import Agent5
 # from OsoyooCarEnacter import OsoyooCarEnacter
 
-#TEST MODIF 3
 class Agent:
     def __init__(self, _hedonist_table):
         """ Creating our agent """
         self.hedonist_table = _hedonist_table
         self._action = None
         self.anticipated_outcome = None
+        self.cycle = 0
+        self.data = None
+        self.etat = "Content"
 
     def action(self, outcome):
         """ tracing the previous cycle """
@@ -21,11 +25,36 @@ class Agent:
                   ", Anticipation: " + str(self.anticipated_outcome) +
                   ", Outcome: " + str(outcome) +
                   ", Satisfaction: (anticipation: " + str(self.anticipated_outcome == outcome) +
+                  ", Etat: " + str(self.etat) + ")" +
                   ", valence: " + str(self.hedonist_table[self._action][outcome]) + ")")
 
         """ Computing the next action to enact """
         # TODO: Implement the agent's decision mechanism
-        self._action = 0
+        if self._action is None:
+            self._action = 0
+        if self.data != outcome:
+            print("\nDifférent ! restart")
+            self.cycle = 0
+            self.data = outcome
+            print('\n')
+        if self.cycle < 5:
+            self.data = outcome
+            self.etat = "Content"
+            self.cycle += 1
+        if self.cycle == 4:
+            self.etat = "Ennui"
+            if self._action == 0:
+                print("Switch action to 1")
+                self._action = 1
+                self.cycle = 0
+                print('\n')
+            elif self._action == 1:
+                print("Switch action to 0")
+                self._action = 0
+                self.cycle = 0
+                print('\n')
+
+
         # TODO: Implement the agent's anticipation mechanism
         self.anticipated_outcome = 0
         return self._action
@@ -64,13 +93,18 @@ class Environment3:
         return _outcome
 
 
+class Environment4:
+    def outcome(self):
+        return random.randint(0, 1)
+
+
 # TODO Define the hedonist valance of interactions (action, outcome)
 hedonist_table = [[-1, 1], [-1, 1]]
 # TODO Choose an agent
 a = Agent(hedonist_table)
 # a = Agent5(hedonist_table)
 # TODO Choose an environment
-e = Environment1()
+e = Environment4()
 # e = Environment2()
 # e = Environment3()
 # e = TurtleSimEnacter()
@@ -82,4 +116,4 @@ if __name__ == '__main__':
     outcome = 0
     for i in range(70):
         action = a.action(outcome)
-        outcome = e.outcome(action)
+        outcome = e.outcome()
